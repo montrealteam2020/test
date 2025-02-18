f1262f666c6b29ed4382ad9dae642ff7f7d2ffe3
montrealteam2020 <montrealteam2020@gmail.com>
2025-02-18 11:32:06 -0500

    Stream

--- src/main/java/com/demo/application/stream/StreamExample.java
+++ src/main/java/com/demo/application/stream/StreamExample.java
@@ -1,4 +1,14 @@
 package com.demo.application.stream;
 
+import java.util.List;
+
 public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> list = List.of("aaaa","bdca","lknd","dzc","dddddddd");
+
+       List filtredList= list.stream().filter(a-> a.length()>4).toList();
+ System.out.println(filtredList.size());
+
+    }
 }
