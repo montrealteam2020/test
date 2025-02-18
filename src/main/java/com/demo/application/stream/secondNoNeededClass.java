00e9f35992fa9df1e9f3179a6b2613b13e5772c2
montrealteam2020 <montrealteam2020@gmail.com>
2025-02-18 11:33:42 -0500

    Second no needed class

---
+++ src/main/java/com/demo/application/stream/secondNoNeededClass.java
@@ -0,0 +1,8 @@
+package com.demo.application.stream;
+
+public class secondNoNeededClass {
+
+    public static void main(String[] args) {
+        System.out.println("No needed second class");
+    }
+}
